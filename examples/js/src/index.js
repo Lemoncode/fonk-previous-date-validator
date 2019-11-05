daa1002961f94a11c2ce61a49b8952ab5c38c887
Nya Garcia <nyablk97@gmail.com>
2019-11-05 11:13:17 +0100

    feat(index.js): update example  to use Date object

--- examples/js/src/index.js
+++ examples/js/src/index.js
@@ -3,16 +3,20 @@ import { previousDate } from '@lemoncode/fonk-previous-date-validator';
 
 const validationSchema = {
   field: {
-    myField: [previousDate.validator],
+    myField: [
+      {
+        validator: previousDate.validator,
+        customArgs: { date: new Date('2019-03-10') },
+      },
+    ],
   },
 };
 
 const formValidation = createFormValidation(validationSchema);
 
-// TODO: Update example values 'test' and/or 10 if needed
 Promise.all([
   formValidation.validateField('myField', 'test'),
-  formValidation.validateField('myField', 10),
+  formValidation.validateField('myField', new Date('2019-02-10')),
 ]).then(([failedResult, succeededResult]) => {
   document.getElementById('app').innerHTML = `
 <div style="flex-grow: 1;margin-left:2rem;">
@@ -32,7 +36,7 @@ ${JSON.stringify(failedResult, null, 2)}
   <h2>Example with succeeded result:</h2>
 
 <pre>
-formValidation.validateField('myField', 10)
+formValidation.validateField('myField', new Date("2019-02-10"))
 </pre>
 
   <h3>Result: </h3>
